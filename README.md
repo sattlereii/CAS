30a92bc2c816439dcfa429e8eb68c66adc4b7774
sattlereii <eliska.junko@gmail.com>
2025-04-30 15:00:41 +0100

    data vsechny zaznamy

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CAS
 
 nevím jestl itu je vše ale snad jo...zkusíme z tohoto poskládat něco :D
+krom ML tak v datech MSUL je vše už
---
+++ data MSUL/počasí/vítr/sss.py
@@ -0,0 +1,11 @@
+import csv
+
+# Cesta k souboru
+file_path = "vitr - kockov.csv"
+
+with open(file_path, encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        if len(row) >= 4:
+            value = row[-4].replace('.', ',')  # tečku nahradíme čárkou
+            print(value)  # třetí hodnota od konce
